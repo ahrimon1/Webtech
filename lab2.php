2098860107533e0a89d75cfab286e4f6142b642e
ahrimon1 <106304315+ahrimon1@users.noreply.github.com>
2023-02-06 10:10:16 +0600

    Add files via upload

---
+++ lab2.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+		
+	
+    </head>
+    <body>
+        <form>
+		<fieldset>
+		<legend>Registration</legend>
+
+            <label for="name">Name </label>
+            <input type="text" name="name"><br><br>
+            
+            <label for="mail">Email</label>
+            <input type="text" name="mail"><br><br>
+
+			<label for="uname">User Name</label>
+            <input type="text" name="uname"><br><br>
+			
+			<label for="pass">Password</label>
+            <input type="text" name="pass"><br><br>
+		
+			
+			<label for="cpass">Confirm Password </label>
+            <input type="text" name="cpass"><br><br>
+			
+			<fieldset>
+			<legend>Gender</legend>
+			<input type="radio" name="gen" >Male
+			<input type="radio" name="gen" >Female
+			<input type="radio" name="gen" >Other<br><br>
+			</fieldset>
+			
+			<fieldset>
+			<legend>Date of Birth</legend>
+			<input type="text" name= "Day">/
+			<input type="text" name= "Month">/
+			<input type="text" name= "Year">(dd/mm/yyyy)<br><br>
+			</fieldset>
+			<button>Submit</button>
+			<button>Reset</button>			
+			
+				
+			</fieldset>
+        </form>
+    </body>
+</html>
